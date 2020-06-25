e35b505317551e8d7b136ef6a86a98350e518195
Wymor <sebastian.w@liesenberg.biz>
2020-06-25 13:59:21 +0200

    Automatic change to the Project

--- Tutorial 9/Tutorial 9.2.py
+++ Tutorial 9/Tutorial 9.2.py
@@ -57,30 +57,38 @@ def rk4_step(x0, y0, z0, t0, f1, f2, f3, h, f1_args = {}, f2_args = {}, f3_args 
     
     return(xp1, yp1, zp1, tp1)
 
-#rk4 wurde noch nicht umgeschrieben!
-
-def rk4(y0, x0, f, h, n, f_args = {}):
+def rk4(x0, y0, z0, t0, f1, f2, f3, h, n, f1_args = {}, f2_args = {}, f3_args = {}):
     ''' Simple implementation of RK4
         Inputs:
+            x_0    - M x 1 numpy array specifying all variables of the ODE at the current time step
             y_0    - M x 1 numpy array specifying all variables of the ODE at the current time step
-            x_0    - current time step
-            f      - function that calculates the derivates of all variables of the ODE
+            z_0    - M x 1 numpy array specifying all variables of the ODE at the current time step
+            t_0    - current time step
+            f1      - function that calculates the derivates of all variables of the first ODE
+            f1      - function that calculates the derivates of all variables of the second ODE
+            f1      - function that calculates the derivates of all variables of the  third ODE
             h      - time step size
             n      - number of steps
-            f_args - Dictionary of additional arguments to be passed to the function f
+            f1_args - Dictionary of additional arguments to be passed to the function f1
+            f2_args - Dictionary of additional arguments to be passed to the function f2
+            f3_args - Dictionary of additional arguments to be passed to the function f3
         Output:
             yn - N+1 x M numpy array with the results of the integration for every time step (includes y0)
             xn - N+1 x 1 numpy array with the time step value (includes start x0)
     '''
+    xn = np.zeros((n+1, x0.shape[0]))
     yn = np.zeros((n+1, y0.shape[0]))
-    xn = np.zeros(n+1)
+    zn = np.zeros((n+1, z0.shape[0]))
+    tn = np.zeros(n+1)
+    xn[0,:] = x0
     yn[0,:] = y0
-    xn[0] = x0
+    zn[0,:] = z0
+    tn[0] = t0
     
     for n in np.arange(1,n+1,1):
-        yn[n,:], xn[n] = rk4_step(y0 = yn[n-1,:], x0 = xn[n-1], f = f, h = h, f_args = f_args)
+        xn[n,:], yn[n,:], zn[n,:], tn[n] = rk4_step(x0 = xn[n-1,:], y0 = yn[n-1,:], z0 = zn[n-1,:], t0 = tn[n-1], f1 = f1, f2 = f2, f3 = f3, h = h, f1_args = f1_args, f2_args = f2_args, f3_args = f3_args))
         
-    return(yn, xn)
+    return(xn, yn, zn, tn)
 
 # Be advised that the integration can take a while for large values of n (e.g >=10^5).
 
